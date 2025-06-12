467796dcc4a204ff915ec0ec1f6ab19c0f6c5217
gervizdev <guilherme96310@gmail.com>
2025-06-12 15:33:25 -0300

    mudei o testa conexao para funcionar com joptionpane para melhor vizualização

--- prg03GuilhermeSantos/src/main/java/br/com/ifba/atividade12/util/TestaConexao.java
+++ prg03GuilhermeSantos/src/main/java/br/com/ifba/atividade12/util/TestaConexao.java
@@ -1,18 +1,21 @@
 package br.com.ifba.atividade12.util;
 
 import jakarta.persistence.*;
+import javax.swing.JOptionPane;
 
 public class TestaConexao {
-  public static void main(String[] args) {
-    try {
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDB");
-      EntityManager em = emf.createEntityManager();
-      System.out.println("✅ Conectado com sucesso!");
-      em.close();
-      emf.close();
-    } catch (Exception e) {
-      System.out.println("❌ Erro ao conectar:");
-      e.printStackTrace();
+    public static void main(String[] args) {
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDB");
+            EntityManager em = emf.createEntityManager();
+            
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!", "Status da Conexão", JOptionPane.INFORMATION_MESSAGE);
+            
+            em.close();
+            emf.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar: \n" + e.getMessage(), "Status da Conexão", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
     }
-  }
 }
